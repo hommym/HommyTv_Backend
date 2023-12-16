9648bb5069a22efcadfe82f1ef612afea0db598e
hommym <herbertharthur80@gmail.com>
2023-12-16 07:24:14 -0800

    Project clean up for new setup

--- app.js
+++ app.js
@@ -1,31 +1,5 @@
-// api key  5df9d8434a271efeaf152516c002398d
 
 
-const express= require("express")
 
-const app= express()
-
-const imgSlideRouter= require("./router-image-slide.js")
-const movies=require("./router-movies.js")
-const series=require("./router-tv_series.js")
-
-
-app.use("/api/images", imgSlideRouter.router)
-app.use("/api/movies", movies.router)
-app.use("/api/tv_series",series.router)
-
-
-app.all('*',(req,res)=>{
-
-res.status(404)
-res.json({error:"Resource Not found"})
-
-})
-
-
-app.listen(5000,()=>{
-console.log("Server is listening to port 5000");
-
-})
 
 
