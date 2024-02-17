9fde2a4ffd01b3aad1b6a52885cec37c5d91e5f5
hommym <herbertharthur80@gmail.com>
2024-02-17 12:03:12 -0800

    refactored the code

--- Controller/user-data-sync-controller.js
+++ Controller/user-data-sync-controller.js
@@ -12,31 +12,63 @@ const synchrozniseUserData= async (req,res)=>{
     const userData= req.body
     console.log(userData);
 
+    const userAccount= req.userAccount
+    console.log(userAccount)
+
 //    checking if any data is passed for body
    if(!userData.contentTitle){
     throw new BadRequest("No data pass for body")
    }
      if(userDataSync==="wtch-later-sync"){
 
-        await userAccountModel.updateOne({_id:req.userAccount._id},{$push:{watchlater:userData}})
+        if(userAccount.watchlater.length===0){
+            await userAccountModel.updateOne({_id:userAccount._id},{$set:{watchlater:userData}})
+
+        }else{
+            await userAccountModel.updateOne({_id:userAccount._id},{$push:{watchlater:userData}})
+        }
+        
         console.log("Watch later data synchronized");
         res.status(201).send("Data Synchronised With Server")
     }
     else if(userDataSync==="history-sync"){
-        await userAccountModel.updateOne({_id:req.userAccount._id},{$push:{watch_history:userData}})
+
+        if(userAccount.watch_history.length===0){
+            await userAccountModel.updateOne({_id:userAccount._id},{$set:{watch_history:userData}})
+
+        }else{
+
+            await userAccountModel.updateOne({_id:userAccount._id},{$push:{watch_history:userData}})
+        }
+        
         console.log("History data synchronized");
         res.status(201).send("Data Synchronised With Server")
 
     }
     else if(userDataSync==="playlist-sync"){
-        await userAccountModel.updateOne({_id:req.userAccount._id},{$push:{play_list:userData}})
+
+        if(userAccount.play_list.length===0){
+            await userAccountModel.updateOne({_id:userAccount._id},{$set:{play_list:userData}})
+        }
+        else{
+            await userAccountModel.updateOne({_id:req.userAccount._id},{$push:{play_list:userData}})
+        }
+       
         console.log("PlayList data synchronized");
         res.status(201).send("Data Synchronised With Server")
 
     }
 
     else if(userDataSync==="fav-sync"){
-        await userAccountModel.updateOne({_id:req.userAccount._id},{$push:{favorite:userData}})
+
+        if(userAccount.favorite.length===0){
+
+            await userAccountModel.updateOne({_id:userAccount._id},{$set:{favorite:userData}})
+        }
+        else{
+            await userAccountModel.updateOne({_id:userAccount._id},{$push:{favorite:userData}})
+        }
+     
         console.log(" Favorite data synchronized");
         res.status(201).send("Data Synchronised With Server")
     }
